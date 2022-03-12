b23cb59e0840267a7129eaf1e9617a5978305ca9
ミク 初音 <mikufsgen@outlook.com>
2022-03-12 21:51:18 +0800

    更改了conf文件地址

--- includes/defines.h
+++ includes/defines.h
@@ -2,7 +2,7 @@
 #define TOOL_VERSION "0.3.2"
 #define AUTHUR "¶¹×ÓDoge"
 #define URL "http://www.douzidoge.cn"
-#define DEVICE_UPDATE_CONF_URL "http://www.douzidoge.cn/deviceupdate/deviceupdateconf_v0.3"
+#define DEVICE_UPDATE_CONF_URL "http://www.douzidoge.cn/deviceupdate/deviceupdateconf_v0.3.2"
 #define DEVICE_UPDATE_CONF "deviceupdateconf"
 #define ESPTOOL_NAME L"esptool.exe"
 
